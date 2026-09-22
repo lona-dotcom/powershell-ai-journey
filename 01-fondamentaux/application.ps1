34e70fa021d0bb0be0c5ef3b7ae6a7c9cf82473c
RAZANAKOTO Lovatahiana Nomenjanahary <lonafstgi@gmail.com>
2026-09-22 17:39:55 +0300

    exo-modification

--- 01-fondamentaux/application.ps1
+++
@@ -1,4 +0,0 @@
-$prenom="Lovatahiana"
-$anneeNaissance=1987
-$villeActuelle="Toamasina"
-Write-Output "Bonjour! Je m'appelle $prenom, je suis né en $anneeNaissance et j'habite actuellement à $villeActuelle avant d'immigrer en Allemagne."
--- 01-fondamentaux/modification.ps1
+++
@@ -1,4 +0,0 @@
-$prixHT = 100
-$tauxTVA = 0.20
-$prixTTC = $prixHT * (1 + $tauxTVA)
-Write-Output "Le prix TTC est : $prixTTC"
