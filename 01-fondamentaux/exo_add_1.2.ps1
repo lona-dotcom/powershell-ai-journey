cbe182e05189210eaab0bad0b8c40976511b38a0
RAZANAKOTO Lovatahiana Nomenjanahary <lonafstgi@gmail.com>
2026-09-24 20:33:19 +0300

    exo-additionnel 1.2

--- 01-fondamentaux/exo_add_1.2.ps1
+++ 01-fondamentaux/exo_add_1.2.ps1
@@ -1,3 +1,7 @@
 $a = 15
 $b = 4
 Write-Output "La somme de $a et $b est : $($a + $b)"
+Write-Output "La différence de $a et $b est : $($a - $b)"
+Write-Output "Le produit de $a et $b est : $($a * $b)"
+Write-Output "Le quotient entier de $a et $b est : $($a / $b)"
+Write-Output "Le reste de la division de $a par $b est : $($a % $b)"
